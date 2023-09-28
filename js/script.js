99a7bea5d767d647c5be589a5b7ca9235c8853ce
Martin_Rio <riof.martin@gmail.com>
2023-09-28 15:38:00 -0300

    Implementación del contenido de Cordoba. Formato del aside.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,50 @@
 window.addEventListener('DOMContentLoaded', () => {
     const HBMENU = document.getElementById("hb_menu");
     const CONTAINER = document.getElementById("nav_list");
+    const ASIDEEXP = document.querySelector(".aside_expand");
+    const ASIDE = document.querySelector("aside");
+    const NAVBAR = document.querySelector("nav");
+
     let lastVPosition = window.scrollY;
     let mobile = window.matchMedia("(max-width: 425px)")
 
-    HBMENU.onblur = hideHBDropdownContent();
+    if (mobile.matches) ASIDE.style.left = "-80vw";
+
+    HBMENU.onblur = hideHBDropdownContent;
+
+    window.onscroll = function adjustNavBar() {
+        if(parseInt(ASIDE.style.left) < 0) {
+            if (mobile.matches) {
+                if (window.scrollY > lastVPosition) {
+                    NAVBAR.style.top = "-100px";
+                } else {
+                    NAVBAR.style.top = "0px";
+                }
+                lastVPosition = window.scrollY;
+            }
+        }
+        hideHBDropdownContent();
+    };
 
     HBMENU.onclick = function displayHBMenu() {
-        if (CONTAINER.clientWidth > 0) {
+        if (parseInt(CONTAINER.style.maxWidth) > 0) {
             hideHBDropdownContent();
         } else {
             CONTAINER.style.maxWidth = "50vw";
             CONTAINER.style.maxHeight = "20rem";
-            CONTAINER.style.backgroundColor = "var(--hbmenu-active-color)";
-            HBMENU.style.backgroundColor = "var(--hbmenu-active-color)";
+            CONTAINER.style.backgroundColor = "var(--bg-highlight-color)";
+            HBMENU.style.backgroundColor = "var(--bg-highlight-color)";
         }
-    }
+    };
 
-    window.onscroll = function adjustNavBar() {
-        const NAVBAR = document.querySelector("nav");
-
-        if (mobile.matches) {
-            if (window.scrollY > lastVPosition) {
-                NAVBAR.style.top = "-100px";
-            } else {
-                NAVBAR.style.top = "0px";
-            }
-            hideHBDropdownContent();
-    
-            lastVPosition = window.scrollY;
+    ASIDEEXP.onclick = function displayAside() {
+        if (parseInt(ASIDE.style.left) >= 0) {
+            ASIDE.style.left = "-80vw";
+            ASIDEEXP.style.left = "0px";
+        } else {
+            NAVBAR.style.top = "0px";
+            ASIDE.style.left = "0px";
+            ASIDEEXP.style.left = "80vw";
         }
     };
 
@@ -42,8 +58,6 @@ window.addEventListener('DOMContentLoaded', () => {
     // function showScrollBar() {
     //     const MAIN = document.querySelector("main");
     //     const SCROLLBAR = MAIN.querySelector("scrollbar");
-
-
     // }
 });
 
