d056583c8476141056bea7171da57d9baa7f8daa
Martin_Rio <riof.martin@gmail.com>
2023-10-04 22:40:47 -0300

    Implementación del contenido de Bariloche.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const ASIDEEXP = document.querySelector(".aside_expand");
     const ASIDE = document.querySelector("aside");
     const NAVBAR = document.querySelector("nav");
+    const SIDEMAPS = document.getElementById("sidemaps");
+    const SIDEMAPSEXP = document.getElementById("sidemaps_expand");
 
     let lastVPosition = window.scrollY;
     let mobile = window.matchMedia("(max-width: 425px)")
@@ -38,13 +40,24 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     ASIDEEXP.onclick = function displayAside() {
+        let icon = ASIDEEXP.querySelector("i");
         if (parseInt(ASIDE.style.left) >= 0) {
             ASIDE.style.left = "-80vw";
             ASIDEEXP.style.left = "0px";
+            icon.style.transform = "scaleX(1)";
         } else {
             NAVBAR.style.top = "0px";
             ASIDE.style.left = "0px";
             ASIDEEXP.style.left = "80vw";
+            icon.style.transform = "scaleX(-1)";
+        }
+    };
+
+    SIDEMAPSEXP.onclick = function displaySideMaps() {
+        if (parseInt(SIDEMAPS.style.right) >= 0) {
+            SIDEMAPS.style.right = "-40vw";
+        } else {
+            SIDEMAPS.style.right = "0px";
         }
     };
 
@@ -59,5 +72,42 @@ window.addEventListener('DOMContentLoaded', () => {
     //     const MAIN = document.querySelector("main");
     //     const SCROLLBAR = MAIN.querySelector("scrollbar");
     // }
+
+    // #region Fooling around
+    const scContainers = document.getElementsByClassName("showcase_container");
+    let containerCount = scContainers.length;
+    let slicesTop = [];
+    let slicesBottom = [];
+    let offsetTop = 0;
+    let offsetBottom = 0;
+
+    for (let i = 1; i <= containerCount; i++) {
+        offsetTop = (100 / containerCount * i) + 5;
+        offsetBottom = (100 / containerCount * i) - 5;
+        slicesTop.push(offsetTop);
+        slicesBottom.push(offsetBottom);
+    }
+    
+    for (let i = 1; i <= containerCount; i++) {
+        const container = scContainers[i - 1];
+        if (i == 1) {
+            container.style.clipPath = `polygon(0 0, ${slicesTop[i - 1]}% 0, ${slicesBottom[i - 1]}% 100%, 0 100%)`;
+        } else if (i == containerCount) {
+            container.style.clipPath = `polygon(${slicesTop[i - 2]}% 0, 100% 0, 100% 100%, ${slicesBottom[i - 2]}% 100%)`;
+        } else {
+            container.style.clipPath = `polygon(${slicesTop[i - 2]}% 0, ${slicesTop[i - 1]}% 0, ${slicesBottom[i - 1]}% 100%, ${slicesBottom[i - 2]}% 100%)`;
+        }
+    }
+    // #endregion
+
+    /*
+    top left: 0 0
+    top right: 100 0
+    bottom right: 100 100
+    bottom left: 0 100
+
+    first:
+    top right: (100 / count) 0
+    */
 });
 
--- js/templates.js
+++ js/templates.js
@@ -19,13 +19,13 @@ let navTemplate = `
     </a>
 </ul>
 <div class="nav_search_bar">
-    <input type="text" name="search" placeholder="buscar...">
+    <input type="text" name="search" placeholder="Buscar...">
     <img src="img/icons/search_icon.svg" alt="search_img">
 </div>
 <div id="hb_menu" class="nav_hb_menu_dropdown" tabindex="3">
     <img src="img/icons/hamburger_menu.png" alt="hamburger_menu">
     <ul id="nav_list">
-        <a href="cordoba.html">
+        <a href="cordoba_capital.html">
             <li>Argentina</li>
         </a>
         <a href="brasil.html">
@@ -43,6 +43,82 @@ let navTemplate = `
 
 document.querySelector("nav").innerHTML = navTemplate;
 
+let asideTemplate = `
+<div class="aside_expand" tabindex="4">
+    <i class="fa-solid fa-arrow-right"></i>
+</div>
+<ul class="aside_links">
+    <a href="cordoba.html"><li><strong>Cordoba</strong>
+        <ul>
+            <a href="cordoba_capital.html"><li><b>Capital</b>
+                <ul>
+                    <a href="cordoba_capital.html#stop_2"><li>Parque Kempes</li></a>
+                    <a href="cordoba_capital.html#stop_3"><li>Iglesias
+                        <ul>
+                            <a href="cordoba_capital.html#stop_4"><li>Los Capuchinos</li></a>
+                            <a href="cordoba_capital.html#stop_5"><li>Catedral</li></a>
+                            <a href="cordoba_capital.html#stop_6"><li>Basílica de San Francisco</li></a>
+                        </ul>
+                    </li></a>
+                    <a href="cordoba_capital.html#stop_7"><li>Vinos y Naturaleza</li></a>
+                </ul>
+            </li></a>
+            <a href="calamuchita.html"><li><b>Calamuchita</b>
+                <ul>
+                    <a href="calamuchita.html#stop_1"><li>Paseo Cultural Rio Sauces</li></a>
+                    <a href="calamuchita.html#stop_2"><li>Sendero Cerro Wank</li></a>
+                    <a href="calamuchita.html#stop_3"><li>Reartes</li></a>
+                </ul>
+            </li></a>
+            <a href="ansenuza.html"><li><b>Ansenuza</b>
+                <ul>
+                    <a href="ansenuza.html#stop_1"><li>Pedaleando el Miramar</li></a>
+                    <a href="ansenuza.html#stop_2"><li>Fiestas Rurales</li></a>
+                    <a href="ansenuza.html#stop_3"><li>Mar de Ansenuza</li></a>
+                </ul>
+            </li></a>
+        </ul>
+    </li></a>
+
+    <a href="bariloche.html"><li><strong>Bariloche</strong></a>
+        <ul>
+            <li><b>Temporada de Invierno</b>
+                <ul>
+                    <a href="cerro_catedral.html"><li>Cerro Catedral</li></a>
+                    
+                    <a href="cerro_otto.html"><li>Cerro Otto</li></a>
+                    <ul>
+                        <a href="cerro_otto.html#stop1"><li>Winter Park</li></a>
+                        <a href="cerro_otto.html#stop2"><li>Piedras Blancas</li></a>
+                        <a href="cerro_otto.html#stop3"><li>EUCA</li></a>
+                        <a href="cerro_otto.html#stop4"><li>Refugio Berghof</li></a>
+                        <a href="cerro_otto.html#stop5"><li>Centro de Esquí Nórdico</li></a>
+                    </ul>
+                </ul>
+            </li>
+            <li><b>Temporada de Verano</b>
+                <ul>
+                    <a href="lago_nahuel_huapi.html"><li>Lago Nahuel Huapi</li></a>
+                    
+                    <a href="lago_guillelmo.html"><li>Lago Guillelmo</li></a>
+                </ul>
+            </li>
+            <li><b>Aventuras en Bariloche</b></li>
+            <ul>
+                <a href="aventura.html#stop1"><li>Cabalgatas</li></a>
+                <a href="aventura.html#stop2"><li>Escaladas</li></a>
+                <a href="aventura.html#stop3"><li>Mountain Bike</li></a>
+                <a href="aventura.html#stop4"><li>Parapente</li></a>
+            </ul>
+        </ul>
+    </li>
+</ul>
+`
+
+const ASIDE = document.querySelector("aside");
+if(ASIDE != null)
+    ASIDE.innerHTML = asideTemplate;
+
 let footerTemplate = `
 <div class="social_links">
     <a href="facebook" target="_blank"><img src="img/icons/facebook.png" alt="facebook"> Facebook</a>
@@ -53,13 +129,7 @@ let footerTemplate = `
     <a href="contact.html">Contacto</a>
 </div>
 <div class="source_links">
-    <!-- source link to logo -->
-    <!-- <a href="https://www.flaticon.es/iconos-gratis/turismo" title="turismo iconos">Turismo iconos creados por Good Ware - Flaticon</a> -->
-    <a href="https://www.flaticon.es/iconos-gratis/boleto" title="boleto iconos">Boleto iconos creados por Eucalyp - Flaticon</a>
-    <a href="https://www.flaticon.es/iconos-gratis/facebook" title="facebook iconos">Facebook iconos creados por Freepik - Flaticon</a>
-    <a href="https://www.flaticon.es/iconos-gratis/gorjeo" title="gorjeo iconos">Gorjeo iconos creados por Roundicons - Flaticon</a>
-    <a href="https://www.flaticon.es/iconos-gratis/instagram" title="instagram iconos">Instagram iconos creados por Freepik - Flaticon</a>
-    <a href="https://www.flaticon.com/free-icons/hamburger" title="hamburger icons">Hamburger icons created by feen - Flaticon</a>
+    <a href="references.html">Puedes encontrar los links de referencia aqui</a>
 </div>
 `
 
